f3f0a464474ea557a1f9ccd8e0caa62d5b5bd0cf
Oltion Gashi <oltion.gashi@mesh-com.at>
2022-06-28 20:07:32 +0200

    generate necessary configs

---
+++ config/i18next.ts
@@ -0,0 +1,18 @@
+import Env from '@ioc:Adonis/Core/Env'
+import { I18nextConfig } from '@ioc:I18n/Next'
+
+const i18nConfig: I18nextConfig = {
+  // MFR: removed english. It is no requirement ATM
+  supportedLngs: ['al', 'en'],
+  fallbackLng: 'en',
+  ns: ['common', 'validation'],
+  defaultNS: 'common',
+  saveMissing: true,
+  interpolation: {
+    prefix: '${{',
+    suffix: '}}$',
+  },
+  debug: Env.get('I18N_DEBUG') ?? false,
+}
+
+export default i18nConfig
