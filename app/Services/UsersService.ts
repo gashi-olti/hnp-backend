4c4a195704dc3ea1a9d72be19bf419ffc96cac2d
Oltion Gashi <oltion.gashi@mesh-com.at>
2022-08-14 20:27:48 +0200

    send email when company sign ups

--- app/Services/UsersService.ts
+++ app/Services/UsersService.ts
@@ -1,18 +1,22 @@
 import { Exception } from '@adonisjs/core/build/standalone'
 import { AuthContract } from '@ioc:Adonis/Addons/Auth'
 import Logger from '@ioc:Adonis/Core/Logger'
+import Env from '@ioc:Adonis/Core/Env'
 import i18next from '@ioc:I18n/Next'
 import User from 'App/Models/User'
 import AuthService from './AuthService'
 import RegisterValidator from 'App/Validators/RegisterValidator'
 import CompaniesService from './Companies'
+import EmailService from './EmailService'
+import { getUserName } from 'Utils/utils'
 
 const authService = new AuthService()
 
 export default class UsersService {
-  private companiesService: CompaniesService = new CompaniesService()
-
   private readonly loginAfterRegister = true
+
+  private companiesService: CompaniesService = new CompaniesService()
+  private emailService: EmailService = new EmailService()
 
   public async register(auth: AuthContract, body: RegisterValidator['schema']['props']) {
     const { profile_type: profileType, ...data } = body
@@ -22,7 +26,24 @@ export default class UsersService {
         const user = await User.create(data)
         const company = await this.companiesService.createCompanyProfile()
 
+        const name = await getUserName(user)
+
         await user.merge({ companyId: company.id }).save()
+        await this.emailService.sendCompany(
+          data.email,
+          i18next.t('common:registration subject'),
+          'register',
+          {
+            user,
+            args: {
+              name,
+              title: i18next.t('common:registration'),
+              url: `${Env.get('FRONTEND_URL')}/verify?token=${user.verificationToken}&email=${
+                user.email
+              }`,
+            },
+          }
+        )
 
         return this.loginAfterRegister ? authService.login(auth, user.id) : { success: true }
       }
