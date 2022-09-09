631e7ccc03108f408685e6769efb146024212fec
Oltion Gashi <oltion.gashi@mesh-com.at>
2022-09-09 15:31:53 +0200

    create profile update function

--- app/Controllers/Http/CompaniesController.ts
+++ app/Controllers/Http/CompaniesController.ts
@@ -1,10 +1,16 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import CompaniesService from 'App/Services/Companies'
+import CompaniesService from 'App/Services/CompaniesService'
+import { CompanyProfileValidator } from 'App/Validators/CompanyProfileValidator'
 
 const companiesService = new CompaniesService()
 
 export default class CompaniesController {
   public async getCompany(ctx: HttpContextContract) {
     return companiesService.getCompany(ctx.auth)
+  }
+
+  public async updateCompanyProfile(ctx: HttpContextContract) {
+    const data = await ctx.request.validate(CompanyProfileValidator)
+    return companiesService.updateCompanyProfile(ctx.auth, data)
   }
 }
--- app/Services/CompaniesService.ts
+++ app/Services/CompaniesService.ts
@@ -1,17 +1,24 @@
 import { Exception } from '@adonisjs/core/build/standalone'
 import { AuthContract } from '@ioc:Adonis/Addons/Auth'
+import { ImageEntities } from '@ioc:Adonis/Addons/ImageConfig'
+import Config from '@ioc:Adonis/Core/Config'
 import Logger from '@ioc:Adonis/Core/Logger'
 import i18next from '@ioc:I18n/Next'
 import Company from 'App/Models/Company'
 import User from 'App/Models/User'
+import { CompanyProfileValidator } from 'App/Validators/CompanyProfileValidator'
+import MediaService from './MediaService'
 // import AuthService from './AuthService'
 
 // const authService = new AuthService()
+
+const imageEntities: ImageEntities = Config.get('imageConfig.entities')
 
 export default class CompaniesService {
   public async getCompany(auth: AuthContract) {
     const user = auth.user as User
     const company = await user.related('company').query().firstOrFail()
+
     try {
       return {
         ...company.serialize(),
@@ -26,5 +33,50 @@ export default class CompaniesService {
   public async createCompanyProfile() {
     const company = Company.create({})
     return company
+  }
+
+  public async updateCompanyProfile(
+    auth: AuthContract,
+    { cover, media, ...data }: CompanyProfileValidator['schema']['props']
+  ) {
+    const user = auth.user as User
+
+    if (!user.allowedProfiles?.includes(User.ProfileTypes.company)) {
+      throw new Exception(i18next.t('company:not company'), 422)
+    }
+
+    try {
+      const company = await user.related('company').query().firstOrFail()
+
+      company.merge(data)
+
+      if (cover) {
+        await MediaService.uploadSingleMedia(
+          company.id,
+          cover,
+          'company',
+          imageEntities.company.cover,
+          null
+        )
+      }
+
+      if (media) {
+        await MediaService.uploadMultipleMedia(
+          company.id,
+          media,
+          'company',
+          imageEntities.company.media,
+          null
+        )
+      }
+
+      await company.save()
+      await company.refresh()
+
+      return company
+    } catch (err) {
+      Logger.error('Error updating company profile: %s', err.message)
+      throw new Exception(i18next.t('company:error updating company profile'))
+    }
   }
 }
